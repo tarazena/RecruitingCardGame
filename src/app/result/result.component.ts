d8abebb3a046ba7d9a057bdf8e1307f5f8b0c9ad
tarazena <tarazena@gmail.com>
2017-04-11 13:41:26 -0400

    More changes to log

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -9,16 +9,37 @@ import { cards, cardsOrSumQuestions } from "../objects/mock-questions";
   styleUrls: ['./result.component.css']
 })
 export class ResultComponent implements OnInit {
-  
+
   cardOptions = cards;
   shapes = cardsOrSumQuestions.filter(x => x.value === 1);
-  
+
   guessCards: Card[];
-    constructor(private cardsService: CardsService) { 
+  constructor(private cardsService: CardsService) {
   }
 
   ngOnInit() {
-    this.guessCards = [new Card, new Card,new Card,new Card,new Card]
+    this.guessCards = [new Card, new Card, new Card, new Card, new Card]
+  }
+
+  guess(s1, n1, s2, n2, s3, n3, s4, n4, s5, n5) {
+    // console.log(this.guessCards);
+    this.guessCards = [{
+      number: n1,
+      shape: s1
+    }, {
+      number: n2,
+      shape: s2
+    }, {
+      number: n3,
+      shape: s3
+    }, {
+      number: n4,
+      shape: s4
+    }, {
+      number: n5,
+      shape: s5
+    }];
+    console.log(this.guessCards);
   }
 
 }
